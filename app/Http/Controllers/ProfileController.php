a61431620e0dbe6a6bb869c3bcc0475dd0e3c3a4
ash <uashvaldez@gmail.com>
2020-04-26 11:27:20 +0800

    Upload File

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,113 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use App\Profile;
+use Illuminate\Http\Request;
+  
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $profiles = Profile::latest()->paginate(5);
+  
+        return view('profiles.index',compact('profiles'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+   
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('profiles.create');
+    }
+  
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'contact' => 'required',
+            'email' => 'required',
+            'birthdate' => 'required',
+        ]);
+  
+        Profile::create($request->all());
+   
+        return redirect()->route('profiles.index')
+                        ->with('success','Profile created successfully.');
+    }
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profile $profile)
+    {
+        return view('profiles.show',compact('profile'));
+    }
+   
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profile $profile)
+    {
+        return view('profiles.edit',compact('profile'));
+    }
+  
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profile $profile)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'contact' => 'required',
+            'email' => 'required',
+            'birthdate' => 'required',
+        ]);
+  
+        $profile->update($request->all());
+  
+        return redirect()->route('profiles.index')
+                        ->with('success','Profile updated successfully');
+    }
+  
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profile $profile)
+    {
+        $profile->delete();
+  
+        return redirect()->route('profile.index')
+                        ->with('success','Profile deleted successfully');
+    }
+}
