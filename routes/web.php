ec0803d7d461368c1ac816b743b7742fa9e93e79
ash <uashvaldez@gmail.com>
2020-04-27 14:49:43 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('profiles.index');
 });
 Route::resource('profiles','ProfileController');
